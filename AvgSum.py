a777da316a264650925ee776eb3e3f92ce4bff5e
flotr18 <trouilletflorent@outlook.com>
2025-02-04 11:00:27 +0100

    Added Sum & Avg

--- AvgSum.py
+++ AvgSum.py
@@ -1,10 +1,14 @@
 import random
 
-def avg():
+
+def AvgSum():
     MyArray = []
     for i in range(10):
         i += 1
         MyArray.append(random.randint(1, 100))
-    print(MyArray)
+    SumArray = sum(MyArray)
+    AvgArray = sum(MyArray) / len(MyArray)
+    print(AvgArray)
+    print(SumArray)
 
-avg()
+AvgSum()
